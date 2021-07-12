1f4b79036aebdb53e7b245d1deee2663f3e72bcf
alinguyen <alinguyen197@gmail.com>
2021-07-12 23:46:29 +0700

    done render list movie UI

--- src/components/MovieCard/MovieCard.jsx
+++ src/components/MovieCard/MovieCard.jsx
@@ -1,12 +1,60 @@
 import React from "react";
+import Time from "react-time-format";
 
 export default function MovieCard(props) {
+  const { value } = props;
+  
+  const avc = value.trailer;
+  console.log(avc);
+  let timeMovie = value.ngayKhoiChieu;
+  let formatTime = new Date(timeMovie);
   return (
-    <div className="card text-left">
-      <img className="card-img-top" src="holder.js/100px180/" alt />
-      <div className="card-body">
-        <h4 className="card-title">Title</h4>
-        <p className="card-text">Body</p>
+    <div className="moviecard">
+      <div className="card text-left">
+        <img
+          className="card-img-top"
+          style={{ width: "100%", height: "350px" }}
+          src={value.hinhAnh}
+          alt=""
+        />
+        <div className="bodymovie">
+          <p className="titlemovie">
+            <span>
+              <i class="fas fa-film"></i>
+            </span>
+            {value.tenPhim}
+          </p>
+          <p className="timemovie">
+            <span>
+              <i class="far fa-clock"></i>
+            </span>
+            <Time value={formatTime} format="hh:mm - DD/MM/YY" />
+          </p>
+        </div>
+        <div className="rating">
+          {value.danhGia}
+          {value.danhGia < 10 ? (
+            <div className="rating-star-incr">
+              <i class="fas fa-star"></i>
+              <i class="fas fa-star"></i>
+              <i class="fas fa-star"></i>
+              <i class="far fa-star"></i>
+              <i class="far fa-star"></i>
+            </div>
+          ) : (
+            <div className="rating-star-discr">
+              <i class="fas fa-star"></i>
+              <i class="fas fa-star"></i>
+              <i class="fas fa-star"></i>
+              <i class="fas fa-star"></i>
+              <i class="fas fa-star"></i>
+            </div>
+          )}
+        </div>
+        <div className="moviebooking">
+          <a href="#">VIEW DETAIL</a>
+        </div>
+       
       </div>
     </div>
   );
--- src/components/RenderMoiveList/RenderMovieList.jsx
+++ src/components/RenderMoiveList/RenderMovieList.jsx
@@ -1,19 +1,110 @@
 import React, { useEffect } from "react";
+import Slider from "react-slick";
 import { useSelector, useDispatch } from "react-redux";
 import { GET_MOVIE_LIST } from "../../redux/const/movieListConst";
+import MovieCard from "../MovieCard/MovieCard";
 
 export default function RenderMovieList() {
   const dispatch = useDispatch();
-  // const { movieList } = useSelector((state) => state.movieListReducer);
-  // console.log(movieList);
+  const { movieList } = useSelector((state) => state.movieListReducer);
+  console.log(movieList);
   useEffect(() => {
     dispatch({ type: GET_MOVIE_LIST });
+   
   }, []);
 
-  //render HTML của component này
-  return (
-    <div className="text-center">
-      <h1>Render Movie List - Test Data Get Movie List</h1>
-    </div>
-  );
+  const render = () => {
+    var settings = {
+      dots: false,
+      infinite: false,
+      speed: 2000,
+      slidesToShow: 4,
+      slidesToScroll: 4,
+      initialSlide: 0,
+      rows: 2,
+      arrows: true,
+      responsive: [
+        {
+          breakpoint: 1024,
+          settings: {
+            slidesToShow: 2,
+            slidesToScroll: 2,
+            infinite: true,
+            dots: false,
+          },
+        },
+        {
+          breakpoint: 600,
+          settings: {
+            slidesToShow: 2,
+            slidesToScroll: 2,
+            initialSlide: 2,
+          },
+        },
+        {
+          breakpoint: 480,
+          settings: {
+            slidesToShow: 1,
+            slidesToScroll: 1,
+          },
+        },
+      ],
+    };
+    return (
+      <section id="rendermovielist">
+        <div className="">
+          <div className="movielist-inner">
+            {/* Nav pills */}
+            <ul className="nav nav-pills ">
+              <li className="nav-item">
+                <a className="nav-link active" data-toggle="pill" href="#home">
+                  Đang Chiếu
+                </a>
+              </li>
+              <li className="nav-item">
+                <a className="nav-link" data-toggle="pill" href="#menu1">
+                  Sắp Chiếu
+                </a>
+              </li>
+            </ul>
+            {/* Tab panes */}
+            <div className="tab-content">
+              <div className="tab-pane container active" id="home">
+                <Slider {...settings}>
+                  {movieList.map((value, index) => {
+                    return (
+                      <div key={index}>
+                        <div className=" movielist-item">
+                          <div className="col-12">
+                            <MovieCard value={value} />
+                          </div>
+                        </div>
+                      </div>
+                    );
+                  })}
+                </Slider>
+              </div>
+              <div className="tab-pane container fade" id="menu1">
+                  <Slider {...settings}>
+                  {movieList.map((value, index) => {
+                    return (
+                      <div key={index}>
+                        <div className=" movielist-item">
+                          <div className="col-12">
+                            <MovieCard value={value} />
+                          </div>
+                        </div>
+                      </div>
+                    );
+                  })}
+                </Slider>
+              </div>
+            </div>
+          </div>
+        </div>
+      </section>
+    );
+  };
+
+  return render();
 }

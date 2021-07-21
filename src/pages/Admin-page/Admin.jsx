3ed6d4c397f2bd41583bb7fab167249f8554ac1c
alinguyen197 <65749387+alinguyen197@users.noreply.github.com>
2021-07-21 14:59:03 +0700

    fix navbar admin page

--- src/pages/Admin-page/Admin.jsx
+++ src/pages/Admin-page/Admin.jsx
@@ -15,93 +15,30 @@ export default function Admin() {
   return (
     <section id="admin">
       <div style={{ height: 60 }}></div>
-      <div className="admin-menu">
-        <div className="navigation">
-          <ul>
-            <li>
-              <a href="">
-                <span className="icon">
-                  <img src={logo} alt="" className="imageIcon" />
-                </span>
-                <span className="title">
-                  <h6>
-                    <b> TIX </b> A d m i n
-                  </h6>
-                </span>
+      <div className="row">
+        <div col="col-3">
+          <ul className="nav nav-pills">
+            <li className="nav-item">
+              <a className="nav-link active" data-toggle="pill" href="#home">
+                Movie Management
               </a>
             </li>
-            <li>
-              <a href="">
-                <span className="icon">
-                  <i class="fa fa-home"></i>
-                </span>
-                <span className="title">DashBoard</span>
-              </a>
-            </li>
-            <li>
-              <a href="">
-                <span className="icon">
-                  <i class="fas fa-people-carry"></i>
-                </span>
-                <span className="title">Customer</span>
-              </a>
-            </li>
-            <li>
-              <a href="">
-                <span className="icon">
-                  <i class="fab fa-facebook-messenger"></i>
-                </span>
-                <span className="title">Message</span>
-              </a>
-            </li>
-            <li>
-              <a href="">
-                <span className="icon">
-                  <i class="fas fa-info-circle"></i>
-                </span>
-                <span className="title">Help</span>
-              </a>
-            </li>
-            <li>
-              <a href="">
-                <span className="icon">
-                  <i class="fas fa-cogs"></i>
-                </span>
-                <span className="title">Setting</span>
-              </a>
-            </li>
-            <li>
-              <a href="">
-                <span className="icon">
-                  <i class="fas fa-key"></i>
-                </span>
-                <span className="title">Password</span>
-              </a>
-            </li>
-            <li>
-              <a href="">
-                <span className="icon">
-                  <i class="fa fa-sign-out-alt"></i>
-                </span>
-                <span className="title">Sign Out</span>
+            <li className="nav-item">
+              <a className="nav-link" data-toggle="pill" href="#menu1">
+                User Management
               </a>
             </li>
           </ul>
         </div>
-        <div className="main">
-          <div className="topbar">
-            <div className="toggle" onClick={() => handleToggle()}>
-              <i class="fa fa-bars" aria-hidden="true"></i>
+        <div col="col-9">
+          <div className="tab-content">
+            <div className="tab-pane container active" id="home">
+              <Dashboard />
             </div>
-
-            <div className="user">
-              <p>Xin chào</p>
-              <img src={logo} alt="" />
+            <div className="tab-pane container fade" id="menu1">
+              <User />
             </div>
           </div>
-
-          <Dashboard />
-          {/* <User /> */}
         </div>
       </div>
     </section>

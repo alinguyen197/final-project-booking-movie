577c7c0a0f441d8917c3e82396ec4294f882ad4c
alinguyen197 <65749387+alinguyen197@users.noreply.github.com>
2021-07-25 15:52:37 +0700

    fix admin movie management

---
+++ src/pages/UserProfile-page/UserProfile.jsx
@@ -0,0 +1,14 @@
+import React, { Component } from "react";
+
+class UserProfile extends Component {
+  render() {
+    return (
+      <div>
+        <div style={{ height: 60 }}></div>
+        <div></div>
+      </div>
+    );
+  }
+}
+
+export default UserProfile;
